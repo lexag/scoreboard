da6b8c3814f89e4ae847944f1c8af8808266e14c
lexag <lexa.grebnets@gmail.com>
2023-02-09 09:58:39 +0100

    unsuccessful build to exe

---
+++ dist/web/main.js
@@ -0,0 +1,27 @@
+
+
+function setScoreboard(p1name, p1sub, p1score, p2name, p2sub, p2score, info, matchid) {
+	$('#one > .name').text(p1name);
+	$('#one > .subtitle').text(p1sub);
+	$('#one > .score').text(p1score);
+	$('#two > .name').text(p2name);
+	$('#two > .subtitle').text(p2sub);
+	$('#two > .score').text(p2score);
+	$('p.info').text(info);
+	$('p.match_id').text(matchid);
+	console.log(p1name, p1sub, p1score, p2name, p2sub, p2score, info, matchid)
+}
+eel.expose(setScoreboard)
+
+
+document.addEventListener('keydown', (event) => {
+	var name = event.key;
+	var code = event.code;
+	if(code.toLowerCase() == "space") {
+		$('*').toggleClass('hide');
+	}
+	eel.receiveKeyPress(name, code)
+	console.log(code)
+}, false);
+
+
